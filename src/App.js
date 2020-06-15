c602b464b3f7e73aca1f03b2c76879cd7927535f
MuhammadSirajAbbasi <64827860+Siraj454@users.noreply.github.com>
2020-06-15 17:55:20 +0500

    Added Counter + Background change button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import Message from './message.js';
 import './App.css';
-
 function App() {
+  let [count,setCount]=useState(0);
+  let [isMorning,setMorning]=useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+
+      <h1>good {isMorning ? 'Morning' : 'Night'}</h1>
+
+       
+      <Message counter={count} />
+      <br/><br/><br/><br/>
+
+      <button onClick={()=>setCount(++count)} >Update Counter</button>
+      <button onClick={()=>setCount(0)}> Reset Counter</button><br/><br></br>
+      <button onClick={()=>setMorning(!isMorning)}>Change Time </button>
     </div>
   );
 }
---
+++ src/message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+
+function Message(props) {
+    return(
+        <div>
+        COuter value is updated to {props.counter}    
+        </div>
+    )
+}
+export default Message;
